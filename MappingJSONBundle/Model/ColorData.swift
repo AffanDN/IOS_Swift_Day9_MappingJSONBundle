21a2fb40e68475902bd15c39a29ff4668c77a643
AffanDN <affan03nusli@gmail.com>
2024-04-24 09:34:00 +0700

    Create color form hex string function

---
+++ MappingJSONBundle/Model/ColorData.swift
@@ -0,0 +1,43 @@
+//
+//  ColorData.swift
+//  MappingJSONBundle
+//
+//  Created by Macbook Pro on 24/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorData: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var color: String
+    var image: String
+    
+    // Color Form HEX
+    var uiColor: Color {
+        colorFromHexString(color)
+    }
+    
+    var uiImage: Image {
+        Image(systemName: image)
+    }
+}
+
+// MARK: - MOCKUP DATA
+
+extension ColorData {
+    static func loadColorData() -> [ColorData] {
+        guard let url = Bundle.main.url(forResource: "Colors",
+                withExtension: ".json"), let data = try? Data(contentsOf: url)
+        else {
+            fatalError("Failed to Load JSON Data from Bundle")
+        }
+        do {
+            let decodedColor = try  JSONDecoder().decode([ColorData].self, from: data)
+            return decodedColor
+        } catch {
+            fatalError("Failed to Decode JSON Data from Bundle: \(error.localizedDescription)")
+        }
+    }
+}
---
+++ MappingJSONBundle/Model/ColorFromHex.swift
@@ -0,0 +1,48 @@
+//
+//  ColorFromHex.swift
+//  MappingJSONBundle
+//
+//  Created by Macbook Pro on 24/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+func colorFromHexString(_ hex: String) -> Color {
+    // #273c75
+    // Lakukan proses Trim : membuang spasi diawal dan diakhir string
+    // menjadikan hex stringnya uppercase
+    
+    let hex = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+    
+    // handle jumlah string nya = 6 / 7(Validasi HEX)
+    // - Asumsi Hex Color tanpa Alpha (opacity) = jumlah 6 digit
+    // - handle Hex dengan # jadinya jumlahnya 7 diigit
+    
+    guard hex.count == 6 || (hex.count == 7 && hex.hasPrefix("#")) else {
+        return Color(red: 0, green: 0, blue: 0) // Default Color (Black)
+    }
+    
+    // Proses Konversi HEX
+    // Konversi dari String HEX dari API menjadi Swift Color
+    let hexColor = hex.hasPrefix("#") ? String(hex.dropFirst()) : hex
+    
+    // Ekstrak Komponen Color kedalam sususan RGB (RRGGBB)
+    let redColor = hexColor.prefix(2)
+    let greenColor = hexColor.dropFirst(2).prefix(2)
+    let blueColor = hexColor.suffix(2)
+    
+    // 1. Konversi HEX yang telah didapat ke INTEGER untuk divalidasi kembali
+    // 2. Radix = basis sistem bilangan matematika
+    // 3. Dari konspe radix kita gunakan konversi string 0-9 dan A-Z menjadi Integer
+    
+    guard let r = UInt64(redColor, radix: 16),
+          let g = UInt64(greenColor, radix: 16),
+          let b = UInt64(blueColor, radix: 16) else {
+        return Color(red: 0, green: 0, blue: 0)
+    }
+    
+    let results = Color(red: Double(r), green: Double(g), blue: Double(b) / 255.0)
+    
+    return Color.red
+}
